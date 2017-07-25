d2d54fca0b579db7337ff1139febf109c7736cc6
Asaf Ben Aharon <asafb26@gmail.com>
2017-07-25 16:51:22 +0300

    refactoring for routers path

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,13 +8,11 @@ module.exports = function (app) {
   // Initialize all routes
   Object.keys(routes).forEach(function (routeName) {
     var router = express.Router();
-    // You can add some middleware here 
-    // router.use(someMiddleware);
 
     // Initialize the route to add its functionality to router
     require('./' + routeName)(router);
 
     // Add router to the speficied route name in the app
-    app.use('/' + changeCase.paramCase(routeName), router);
+    app.use('/api/' + changeCase.paramCase(routeName), router);
   });
 };
